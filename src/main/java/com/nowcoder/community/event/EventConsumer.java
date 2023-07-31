f214327b92ad7ff132af347af9b3c22cd4dbb613
leoxu <leoxuwj@163.com>
2023-07-31 16:33:45 +0800

    完成到系统通知

---
+++ src/main/java/com/nowcoder/community/event/EventConsumer.java
@@ -0,0 +1,63 @@
+package com.nowcoder.community.event;
+
+import com.alibaba.fastjson.JSONObject;
+import com.nowcoder.community.entity.Event;
+import com.nowcoder.community.entity.Message;
+import com.nowcoder.community.service.MessageService;
+import com.nowcoder.community.util.CommunityConstant;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class EventConsumer implements CommunityConstant {
+
+    private static final Logger logger = LoggerFactory.getLogger(EventConsumer.class);
+
+    @Autowired
+    private MessageService messageService;
+
+    @KafkaListener(topics = {TOPIC_COMMENT, TOPIC_LIKE, TOPIC_FOLLOW})
+    public void handleCommentMessage(ConsumerRecord record) {
+        if (record == null || record.value() == null) {
+            logger.error("消息的内容为空!");
+            return;
+        }
+
+        Event event = JSONObject.parseObject(record.value().toString(), Event.class);
+        if (event == null) {
+            logger.error("消息格式错误!");
+            return;
+        }
+
+        // 发送站内通知
+        Message message = new Message();
+        message.setFromId(SYSTEM_USER_ID);
+        message.setToId(event.getEntityUserId());
+        message.setConversationId(event.getTopic());
+        message.setCreateTime(new Date());
+
+        // 构造message中content这个字段的内容，内容分为2部分。
+        // 1. 系统通知内容需要的动态内容：发起人的姓名，发起人在什么实体进行点赞或评论，实体的id
+        Map<String, Object> content = new HashMap<>();
+        content.put("userId", event.getUserId());
+        content.put("entityType", event.getEntityType());
+        content.put("entityId", event.getEntityId());
+        // 2. 一些额外的信息也一起存入content，如：帖子的id（因为并不是所有类型的实体都有帖子id，所以存入content里）
+        if (!event.getData().isEmpty()) {
+            for (Map.Entry<String, Object> entry : event.getData().entrySet()) {
+                content.put(entry.getKey(), entry.getValue());
+            }
+        }
+
+        message.setContent(JSONObject.toJSONString(content));
+        messageService.addMessage(message);
+    }
+}
